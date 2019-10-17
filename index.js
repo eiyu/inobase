1424ae02e23e24e8dc859c1b109fe2a37580366f
yuliana <ysiulil22@gmail.com>
2019-10-17 21:59:39 +0700

    add nightmares

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // require('dotenv').config()
-const weapons = require('./weapons')
+const weapons = require('./lib/weapons')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+  console.log('警鐘ハ散華ノ火花ヲ呼ブ(壱), 警鐘ハ散華ノ火花ヲ呼ブ(弐)')
   console.log(`Logged in as ${client.user.tag}! bleh`);
 });
 
