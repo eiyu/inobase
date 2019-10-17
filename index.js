8857785fa8a4a3b4cc24dcc6f8ebc345e747ca7c
yuliana <ysiulil22@gmail.com>
2019-10-17 12:52:17 +0700

    inobase

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+require('dotenv').config()
 const weapons = require('./weapons')
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-console.log(weapons['blade']['Zero\'s Sword'])
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.CLIENT_KEY);
