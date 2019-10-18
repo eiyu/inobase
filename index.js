f83bbec279f5db795364968bcd779f9a37c3507d
yuliana <ysiulil22@gmail.com>
2019-10-18 21:18:01 +0700

    add commands

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
 const weapons = require('./lib/weapons')
+const { isCommand, getCommand, getQueries } = require('./lib/commands')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('警鐘ハ散華ノ火花ヲ呼ブ(壱), 警鐘ハ散華ノ火花ヲ呼ブ(弐)')
   console.log(`Logged in as ${client.user.tag}! bleh`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+  if (isCommand(msg.content)) {
+    console.log('woot',getQueries(msg.content));
+    return;
+  };
+  return;
 });
 
 client.login(process.env.CLIENT_KEY);
