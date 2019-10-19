7c6e280020a6699f708ffd471d8043323ee63cb9
yuliana <ysiulil22@gmail.com>
2019-10-19 07:57:01 +0700

    seting an embed msg

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (isCommand(msg.content)) {
     console.log('woot',getQueries(msg.content).length, getQueries(msg.content)[0]);
-    return;
+    const data = getQueries(msg.content)
+    const buildEmbedForItem = (itemData) => {
+      return new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://discord.js.org/', 'https://discord.js.org/')
+        .setDescription('Some description here')
+        .setThumbnail(`${itemData[1]['picture']}`)
+        .addField('Story Skill', `${itemData[0]['story_skill']}`, true)
+        .addField('Colosseum Skill', `${itemData[0]['colosseum_skill']}`, true)
+        .addField('Aid Skill', `${itemData[0]['aid_skill']}`, true)
+        .setFooter('Some footer text here');
+    };
+
+    return msg.channel.send(buildEmbedForItem(data));
   };
   return;
 });
